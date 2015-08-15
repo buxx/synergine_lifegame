42e36eb3b2bc147f37b48cb02c472ee5dac951c0
Bastien Sevajol <sevajol.bastien@gmail.com>
2015-08-15 16:44:48 +0200

    Revert "SynergyObject: add possibility to force id"
    This reverts commit 0cf566ed0212509dc72ed18ad458e8ce558b6ede.

--- synergine_lifegame/synergy/object/Cell.py
+++ synergine_lifegame/synergy/object/Cell.py
@@ -8,13 +8,13 @@ class Cell(XyzSynergyObject):
     Representation of cell.
     """
 
-    def __init__(self, collection, context, forced_id=None):
+    def __init__(self, collection, context):
         """
 
         :param collection: Collection who contain this Cell
         :param context: The context object
         """
-        super().__init__(collection, context, forced_id)
+        super().__init__(collection, context)
         self._alive = False
         self._alive_since = -1
         # By default, a cell is dead
